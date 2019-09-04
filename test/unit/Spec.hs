f377301df9ad37195c93d693cace151839de61ea
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-09-04 21:01:45 +0200

    Updated type checker tests

--- test/unit/Spec.hs
+++ test/unit/Spec.hs
@@ -1,9 +1,9 @@
 import Logger
 import Types
 import qualified Parser.Main as Parser
-import qualified TypeChecker.Main as TypeChecker
+import qualified TypeChecker.Main as ExprTypeChecker
 
 main :: IO ()
 main =
     runSpec "parser" Parser.run
-    *> runSpec "type-checker" TypeChecker.run
+    *> runSpec "type-checker" ExprTypeChecker.run
--- test/unit/TypeChecker/Main.hs
+++ test/unit/TypeChecker/Main.hs
@@ -1,9 +1,46 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+
 module TypeChecker.Main where
 
 import Logger
 import System.Exit
 import Types
 import Control.Monad.State
+import Pixie.TypeChecker.Types
+import Pixie.Parser.Types hiding (Type(..))
+import Pixie.TypeChecker.Checker
+import Data.Key
+import qualified Data.Text as Text
 
 run :: Spec ()
-run = liftIO (red (putStrLn "Nothing to type-check for now.")) <* put True
+run =
+    sequence_ (mapWithKey testTC testExpressions)
+  where
+    testTC :: Int -> (Expr, Type, Bool) -> Spec ()
+    testTC index (e, t, shouldFail) =
+        let res = runCheck (tcExpr e)
+            indexShow = putStr ("Test " <> show (index + 1) <> " of " <> show (length testExpressions) <> ": ")
+        in  liftIO indexShow
+            *> case res of
+                (x, [])
+                    | x /= t ->
+                        liftIO ((bold . red) (putStrLn "Failed!")
+                            *> red (putStrLn "Cannot unify expr type with expected type"))
+                        *> put True
+                    | otherwise -> liftIO $ do
+                        (bold . green) (putStrLn "Passed!")
+                        blue (putStr (show e) *> putStr " :: " *> print x)
+                (x, errs)
+                    | not shouldFail -> 
+                        liftIO ((bold . red) (putStrLn "Failed!")
+                            *> red (mapM_ print errs))
+                        *> put True
+                    | otherwise ->
+                        liftIO $ (bold . green) (putStrLn "Passed!")
+                            *> green (putStrLn "Type checking errored out as intended:"
+                                *> mapM_ ((*>) (putStr "> ") . print) errs)
+
+testExpressions :: [(Expr, Type, Bool)]
+testExpressions = [ (Lit (LInt 0), TInt, False)
+                  , (VarId "hello", TVoid, True)
+                  , (FunCall "x" [Lit (LInt 1)] `Add` Lit (LFloat $ -1.3), TVoid, True) ]
