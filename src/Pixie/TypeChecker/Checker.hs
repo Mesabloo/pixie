90848ac86a4e8122fde7b0de664ec5b7eda59764
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-09-04 22:13:57 +0200

    Added function call type checking

--- src/Pixie/TypeChecker/Checker.hs
+++ src/Pixie/TypeChecker/Checker.hs
@@ -4,8 +4,6 @@ module Pixie.TypeChecker.Checker where
 
 import Pixie.TypeChecker.Types
 import Control.Monad.State
-import Control.Monad.Reader
-import Control.Monad.Writer
 import Control.Monad.RWS
 import qualified Data.Text as Text
 import qualified Pixie.Parser.Types as P (Type(..))
@@ -13,6 +11,9 @@ import Pixie.Parser.Types hiding (Type(..))
 import qualified Data.Map as Map
 import Text.PrettyPrint.Leijen
 import Control.Monad.Writer
+import Control.Applicative
+import Control.Monad.Reader
+import Pixie.TypeChecker.Errors
 
 tcExpr :: Expr -> Check Type
 tcExpr (Lit l) =
@@ -21,7 +22,29 @@ tcExpr (Lit l) =
             LFloat _ -> TFloat
             LChar _ -> TChar
     in pure $ tcLit l
-tcExpr e = censor (text "Type-checking for expression “" <> text (show e) <> text "” is not yet implemented." :) (pure TVoid)
+tcExpr (VarId name) =
+    lookupEnv name >>= pure
+tcExpr (FunCall name args) =
+    lookupEnv name >>= \case
+        TFun argsT retT ->
+            let checkArgs = do
+                    res <- mapM tcExpr args
+                    if length argsT == length res
+                    then forM_ (zip argsT res) $
+                        \(t1, t2) ->
+                            if t1 /= t2
+                            then censor (makeUnifyError t1 t2 :) (pure TVoid)
+                            else pure TVoid
+                    else censor (makeWrongNumberOfArgsError (Text.unpack name) (length argsT) (length res) :) (pure ())
+            in retT <$ checkArgs
+        _ -> censor (makeNotAFunctionError (Text.unpack name) :) (pure TVoid)
+tcExpr e = censor (text "Type-checking for expression `" <> text (show e) <> text "` is not yet implemented." :) (pure TVoid)
+
+lookupEnv :: Text.Text -> Check Type
+lookupEnv name =
+    asks (Map.lookup name . unwrapTypeEnv) >>= \case
+        Nothing -> censor (makeUnboundIdError (Text.unpack name) :) (pure TVoid)
+        Just t -> pure t
 
 -------------------------------------------------------------------------------------------------------
 
--- src/Pixie/TypeChecker/Types.hs
+++ src/Pixie/TypeChecker/Types.hs
@@ -10,12 +10,15 @@ import Text.PrettyPrint.Leijen
 import Data.Void
 
 data Type = TInt | TFloat | TChar | TVoid
-          | TFun Type [Type]
+          | TFun [Type] Type
     deriving (Eq, Ord, Show)
 
 type TIError = Doc
 
-newtype TypeEnv = TypeEnv (Map.Map String Type)
+newtype TypeEnv = TypeEnv (Map.Map Text Type)
     deriving (Show, Monoid, Semigroup)
+
+unwrapTypeEnv :: TypeEnv -> Map.Map Text Type
+unwrapTypeEnv (TypeEnv m) = m
 
 type Check = RWS TypeEnv [TIError] Int
