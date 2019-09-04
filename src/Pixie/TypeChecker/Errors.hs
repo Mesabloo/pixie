88e9128c4e8e67fd85189783e208eee7a5dc1b25
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-09-04 22:13:28 +0200

    Added an error file for typechecking errors

---
+++ src/Pixie/TypeChecker/Errors.hs
@@ -0,0 +1,18 @@
+module Pixie.TypeChecker.Errors where
+
+import Text.PrettyPrint.Leijen
+import Pixie.TypeChecker.Types
+
+makeWrongNumberOfArgsError :: String -> Int -> Int -> Doc
+makeWrongNumberOfArgsError name expected got =
+    text ("Wrong number of argument provided to function `" <> name
+        <> "`: expected " <> show expected <> ", got " <> show got <> ".")
+
+makeUnboundIdError :: String -> Doc
+makeUnboundIdError name = text ("Undefined variable or function `" <> name <> "`.")
+
+makeNotAFunctionError :: String -> Doc
+makeNotAFunctionError name = text ("`" <> name <> "` is not a function.")
+
+makeUnifyError :: Type -> Type -> Doc
+makeUnifyError t1 t2 = text ("Cannot match type `" <> show t1 <> "` with type `" <> show t2 <> "`.")
