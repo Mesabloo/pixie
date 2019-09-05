4c34c49998244e4316ac04d05ca6e29ce7dd32fc
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-09-05 17:04:43 +0200

    Added some more tests

--- test/unit/ProgramTypeChecker/Main.hs
+++ test/unit/ProgramTypeChecker/Main.hs
@@ -39,4 +39,6 @@ testPrograms = [ (Program [Function "main" Int [] []], True)
                , (Program [Global "x" Float $ Lit (LFloat (-0.0))], False)
                , (Program [Function "f" Void [] [], Global "x" Void $ FunCall "f" []], False)
                , (Program [Function "test2" Int [Var "x" Int] [VarDef "y" Int (Mul (Add (VarId "x") (Lit (LInt 1))) (Lit (LInt 2))), Return (VarId "y")]], False)
-               , (Program [Function "main" Int [] [VarDef "x" Int (Lit (LInt 0)), Return (Lit $ LFloat 0)]], True) ]
+               , (Program [Function "main" Int [] [VarDef "x" Int (Lit (LInt 0)), Return (Lit $ LFloat 0)]], True)
+               , (Program [Function "f" Int [] [Return (Lit $ LInt 0), Return (Lit $ LFloat 0.0)]], True)
+               , (Program [Function "f" Int [] [Return (Lit $ LInt 0), Return (Lit $ LInt 0)]], False) ]
