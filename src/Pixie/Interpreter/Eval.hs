efc6dbd35a9415ac40c4a9da1a5949ddf2a90ade
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-09-05 17:32:56 +0200

    Added operation evaluation

--- src/Pixie/Interpreter/Eval.hs
+++ src/Pixie/Interpreter/Eval.hs
@@ -10,6 +10,7 @@ import Control.Monad.Reader
 import qualified Data.Map as Map
 import Data.Text hiding (zip)
 import Debug.Trace
+import Prelude hiding (div)
 
 evalExpr :: Expr -> Eval Value
 evalExpr (Lit l) =
@@ -25,11 +26,30 @@ evalExpr (FunCall name args) = do
         v@(VFun _ _) -> pure v
         _ -> throwError (text "Incorrect value retrieved, should be a function!")
     let VFun argNames body = vfun
-    vArgs <- forM (zip argNames args) $
-        \(name, expr) -> (name,) <$> evalExpr expr
+    vArgs <- forM (zip argNames args) $ \(name, expr) -> (name,) <$> evalExpr expr
     let argsEnv = Map.fromList vArgs
     local (wrapEvalEnv . (argsEnv `Map.union`) . unwrapEvalEnv) (evalFunStatements body)
+evalExpr (Add e1 e2) =
+    evalOperator add e1 e2
+evalExpr (Sub e1 e2) =
+    evalOperator sub e1 e2
+evalExpr (Mul e1 e2) =
+    evalOperator mul e1 e2
+evalExpr (Div e1 e2) =
+    evalOperator div e1 e2
 evalExpr e = throwError (text "Unimplemented")
+
+evalOperator :: (Value -> Value -> Eval Value) -> Expr -> Expr -> Eval Value
+evalOperator f e1 e2 = do
+    ev1 <- evalExpr e1
+    ev2 <- evalExpr e2
+    checkNumVal ev1 *> checkNumVal ev2
+    f ev1 ev2
+
+checkNumVal :: Value -> Eval ()
+checkNumVal (VInt _) = pure ()
+checkNumVal (VFloat _) = pure ()
+checkNumVal v = throwError (text ("`" <> show v <> "` is not a numeric value!"))
 
 evalFunStatements :: [FunStatement] -> Eval Value
 evalFunStatements [] = pure VVoid
@@ -59,3 +79,24 @@ runEval :: Eval a -> Either EvalError a
 runEval e =
     let r = mempty
     in runExcept $ runReaderT e r
+
+-----------------------------------------------------------------------------------------------------------------
+
+add :: Value -> Value -> Eval Value
+add (VInt i1) (VInt i2) = pure $ VInt (i1 + i2)
+add (VFloat f1) (VFloat f2) = pure $ VFloat (f1 + f2)
+add _ _ = throwError (text "Type checking failed for the `+` operator.")
+
+sub :: Value -> Value -> Eval Value
+sub (VInt i1) (VInt i2) = pure $ VInt (i1 - i2)
+sub (VFloat f1) (VFloat f2) = pure $ VFloat (f1 - f2)
+sub _ _ = throwError (text "Type checking failed for the `-` operator.")
+
+div :: Value -> Value -> Eval Value
+div (VFloat f1) (VFloat f2) = pure $ VFloat (f1 / f2)
+div _ _ = throwError (text "Type checking failed for the `/` operator.")
+
+mul :: Value -> Value -> Eval Value
+mul (VInt i1) (VInt i2) = pure $ VInt (i1 * i2)
+mul (VFloat f1) (VFloat f2) = pure $ VFloat (f1 * f2)
+mul _ _ = throwError (text "Type checking failed for the `*` operator.")
